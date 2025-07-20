844f524a0c8d47a1b9e25d6a51cbdbe264a04b7a
avinash-218 <ravinash218@gmail.com>
2025-07-20 12:46:43 +0530

    removed LangGraph

--- LangGraph-101/8-rag-agent/5_agentic_rag/graph/chains/generation.py
+++
@@ -1,9 +0,0 @@
-from langchain import hub
-from langchain_core.output_parsers import StrOutputParser
-from langchain_ollama import ChatOllama
-
-llm = ChatOllama(model='llama3:8b', temperature=0.2)
-prompt = hub.pull('rlm/rag-prompt')
-
-generation_chain= prompt | llm | StrOutputParser()
-
